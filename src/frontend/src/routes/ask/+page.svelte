381716378d1411a71f36087b2b5fa57f1627ae21
Sofa <Nextil3o3@gmail.com>
2023-02-12 19:42:42 +0100

    fixed finally

--- src/frontend/src/routes/ask/+page.svelte
+++ src/frontend/src/routes/ask/+page.svelte
@@ -1,31 +1,73 @@
 <script>
 	import { onMount } from 'svelte';
-	import Ask from '../../components/Ask.Svelte';
 
 	let mainData = [];
-	let displayName = '';
-	let subState = '';
 
 	onMount(async () => {
 		const data = await fetch('/api/public');
 		mainData = await data.json();
-
-		try {
-			displayName = mainData['display_name']['value'];
-			subState = mainData['submissions_state']['value'];
-		} catch {}
 	});
+
+	function clickAns() {
+		answeredBtn.addEventListener('click', () => {
+			window.location.href = `/answered`;
+		});
+	}
+
+	function clickSend(event) {
+		const questionData = event.getElementById('question');
+		if (questionData.value === '') {
+			questionData.placeholder = `Please enter a question`;
+			setTimeout(() => {
+				questionData.placeholder = `Ask your question here`;
+			}, 800);
+			return;
+		}
+		fetch(`/api/questions?question=${questionData.value}`, { method: 'POST' }).then(
+			(questionData.value = ``)
+		);
+	}
 </script>
 
 <div class="ask">
-	{#if subState}
-		<Ask />
+	{#if mainData['submissions_state']['value']}
+		<img src={mainData['display_image']['value']} alt="profile" class="picture" />
+		<div class="name">Ask {mainData['display_name']['value']}</div>
+		<div class="asking">
+			<input
+				type="text"
+				class="question"
+				id="question"
+				placeholder="Ask your question here"
+				maxlength="100"
+			/>
+			<button id="send" class="button" on:click={clickSend}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="feather feather-send"
+				>
+					<line x1="22" y1="2" x2="11" y2="13" />
+					<polygon points="22 2 15 22 11 13 2 9 22 2" />
+				</svg>
+			</button>
+		</div>
+		<button class="button as" on:click={clickAns}>Answered Questions</button>
 		<div class="info">
 			Built with <a href="https://deta.space/discovery/@sofa/askme">AskMe</a>, powered by
 			<a href="https://deta.space">Deta Space</a>
 		</div>
 	{:else}
-		<div class="message">{displayName} is currently not answering new questions!</div>
+		<div class="message">
+			{mainData['display_name']['value']} is currently not answering new questions!
+		</div>
 	{/if}
 </div>
 
@@ -49,4 +91,65 @@
 		filter: blur(5px);
 	}
 	*/
+
+	.asking {
+		flex-direction: row;
+		display: flex;
+		align-items: center;
+	}
+
+	.name {
+		font-size: 50px;
+		color: var(--text-dark);
+		margin: 5px;
+	}
+
+	.picture {
+		border-radius: 50%;
+		height: 140px;
+		width: 140px;
+	}
+
+	.button {
+		padding: 4px 8px;
+		border: 1px solid var(--highlight-main);
+		border-radius: 4px;
+		background-color: var(--highlight-sec);
+		cursor: pointer;
+		color: var(--text-light);
+		justify-content: center;
+		align-items: center;
+		font-size: 10px;
+		align-self: center;
+		margin: 10px;
+	}
+
+	.button:hover {
+		border-color: var(--text-sec);
+		color: var(--text-main);
+	}
+
+	.button.as {
+		font-size: 15px;
+	}
+
+	.question {
+		padding: 6px 20px;
+		border-radius: 4px;
+		text-align: center;
+		border: 1px solid var(--highlight-main);
+		background-color: var(--highlight-sec);
+		width: 150px;
+	}
+
+	.question:focus {
+		border-color: var(--text-sec);
+		color: var(--text-main);
+		outline: none;
+	}
+
+	svg {
+		height: 15px;
+		width: 15px;
+	}
 </style>
